bf491b3d9dc8d124e9e2f7adf237afe5d03aec6f
Kirill <cyberk1ra@yandex.ru>
2023-01-29 23:17:17 +0300

    выводи данные

---
+++ Model/Connection/Connection.cs
@@ -0,0 +1,14 @@
+﻿using BeautyBrows.Model.ADO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautyBrows.Model.Connection
+{
+	internal static class Connection
+	{
+		public static BeautyBrowsEntities BeautyBrowsEntities = new BeautyBrowsEntities();
+	}
+}
---
+++ View/Screen/User/Welcome.xaml.cs
@@ -0,0 +1,40 @@
+﻿using BeautyBrows.Action.Navigation;
+using BeautyBrows.View.Screen.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BeautyBrows.View.Screen.User
+{
+	/// <summary>
+	/// Логика взаимодействия для Welcome.xaml
+	/// </summary>
+	public partial class Welcome : Page
+	{
+		public Welcome()
+		{
+			InitializeComponent();
+		}
+
+		private void ButtonContinueAsUser_Click(object sender, RoutedEventArgs e)
+		{
+			FrameNavigation.Frame.Navigate(new Service());
+        }
+
+		private void ButtonContinueAsAdmin_Click(object sender, RoutedEventArgs e)
+		{
+			FrameNavigation.Frame.Navigate(new Login());
+        }
+    }
+}
